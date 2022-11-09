5ab8eedbce5474dc65cb6048f93a457f3eb3be3d
root <root@Proline_NBK.localdomain>
2022-11-09 13:27:41 +0200

    malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *s;
-unsigned int i = 0;
-s = malloc(size * sizeof(char));
-if (s == NULL)
-return (NULL);
-if (size == 0)
-return (NULL);
-while (i < size)
-{
-s[i] = c;
-}
-return (s);
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }
